8c349ee8d42ddd590ecb835ba2d8b1585f9906b8
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-12 20:35:45 +0600

    By.ID working

---
+++ practice_locators.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+# 1. By.ID
+driver.find_element(By.ID, "my-text-id").send_keys("Using ID")
+
+time.sleep(5)
