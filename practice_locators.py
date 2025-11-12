8fa8076581fc384d41a44f54615921a483ac4ca5
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-12 20:37:29 +0600

    By.NAME working

--- practice_locators.py
+++ practice_locators.py
@@ -8,4 +8,8 @@ driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 # 1. By.ID
 driver.find_element(By.ID, "my-text-id").send_keys("Using ID")
 
+# 2. By.NAME
+
+driver.find_element(By.NAME, "my-password").send_keys("Password")
+
 time.sleep(5)
