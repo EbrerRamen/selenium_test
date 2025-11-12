84d981f1eff19725322976b4fed7bc67eeb3589c
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-12 20:48:34 +0600

    By.CLASS_NAME working

--- practice_locators.py
+++ practice_locators.py
@@ -9,7 +9,9 @@ driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 driver.find_element(By.ID, "my-text-id").send_keys("Using ID")
 
 # 2. By.NAME
-
 driver.find_element(By.NAME, "my-password").send_keys("Password")
+
+# 3. By.CLASS_NAME
+driver.find_element(By.CLASS_NAME, "form-control").send_keys(" This goes to the first field that has classname of form-control")
 
 time.sleep(5)
