5b8bdc49d26d7eae373887fc17e836dc949eddde
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-13 18:48:57 +0600

    explicit wait - visibility_of_element_located

--- explicit_wait.py
+++ explicit_wait.py
@@ -19,3 +19,18 @@ If the element if found in the DOM before 10 seconds, it returns it immediately.
 if not found within 10 seconds -> TimeoutException.
 """
 element.send_keys("Abrar")
+
+"""
+#visibility_of_element_located
+Waits until the element is:
+1. Present in the DOM, and
+2. Visible (i.e. displayed on the page, not hidden by CSS or off-screen)
+"""
+element = wait.until(
+    EC.visibility_of_element_located((By.ID, "username"))
+)
+"""
+Selenium keeps checking every few milliseconds until the element in visible.
+Once it is, the condition passes and returns the element.
+if it doesn't appear within 10 seconds -> TimeoutException.
+"""
