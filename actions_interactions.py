c77f431cead04613941c9a40961d66d82b30e4c7
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-14 15:09:17 +0600

    Combination of keys like ENTER TAB

--- actions_interactions.py
+++ actions_interactions.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
 driver.get("https://abrar-portfolio-six.vercel.app/")
 
+# send_keys()
+
 name = driver.find_element(By.ID, "name")
 name.send_keys("Selenium")
 
 name.clear() # Clear before typing (recommended)
-name.send_keys("Abrar")
+name.send_keys("Abrar", Keys.TAB)
+
+email = driver.find_element(By.ID, "email")
+email.send_keys("xyz@xyz.com")
+
+message = driver.find_element(By.ID, "message")
+message.send_keys("Testing", Keys.ENTER)
 
 time.sleep(3)
 
